0fdb3976420571be157826dffe8b282e16ac6ea9
Stephen Kang <ki6080@gmail.com>
2020-11-08 22:59:28 +0900

    Implement adaptive thread count adjust

--- core/include/pipepp/thread_pool.hxx
+++ core/include/pipepp/thread_pool.hxx
@@ -50,10 +50,35 @@ private:
 
 public:
     std::chrono::milliseconds launch_timeout_ms{1000};
+    std::chrono::milliseconds stall_check_period{5};
+    std::chrono::milliseconds stall_wait_tolerance{5};
+
+private:
+    struct worker_desc {
+        struct atomic_bool_wrap_t {
+            std::atomic_bool value;
+
+            atomic_bool_wrap_t() = default;
+            atomic_bool_wrap_t(const atomic_bool_wrap_t& other) = delete;
+            atomic_bool_wrap_t(atomic_bool_wrap_t&& other) noexcept
+            {
+                value = other.value.load();
+            }
+            atomic_bool_wrap_t& operator=(const atomic_bool_wrap_t& other) = delete;
+            atomic_bool_wrap_t& operator=(atomic_bool_wrap_t&& other) noexcept
+            {
+                value = other.value.load();
+                return *this;
+            }
+        };
+        std::thread thread;
+        atomic_bool_wrap_t disposer;
+        std::chrono::system_clock::time_point last_active;
+    };
 
 private:
     safe_queue<std::function<void()>> tasks_;
-    std::vector<std::pair<std::shared_ptr<std::atomic_bool>, std::thread>> workers_;
+    std::vector<worker_desc> workers_;
     mutable std::mutex worker_lock_;
 
     std::condition_variable event_wait_;
@@ -62,84 +87,6 @@ private:
     std::atomic_size_t num_working_workers_;
     size_t const worker_limit_;
 };
-
-inline thread_pool::thread_pool(size_t task_queue_cap_, size_t num_workers, size_t worker_limit) noexcept
-    : tasks_(task_queue_cap_)
-    , worker_limit_(worker_limit)
-{
-    resize_worker_pool(num_workers);
-}
-
-inline thread_pool::~thread_pool()
-{
-    pop_workers__(workers_.size());
-}
-
-inline void thread_pool::resize_worker_pool(size_t new_size)
-{
-    if (new_size == 0) {
-        throw std::invalid_argument{"Size 0 is not allowed"};
-    }
-
-    std::lock_guard<std::mutex> lock(worker_lock_);
-    if (new_size > workers_.size()) {
-        new_size = std::min(worker_limit_, new_size);
-        while (new_size != workers_.size()) {
-            try_add_worker__();
-        }
-    }
-    else if (new_size < workers_.size()) {
-        pop_workers__(workers_.size() - new_size);
-    }
-}
-
-inline size_t thread_pool::num_workers() const
-{
-    std::lock_guard<std::mutex> lock(worker_lock_);
-    return workers_.size();
-}
-
-inline bool thread_pool::try_add_worker__()
-{
-    auto disposer = std::make_shared<std::atomic_bool>(false);
-    if (workers_.size() >= worker_limit_) {
-        return false;
-    }
-
-    auto worker = [this, disposer]() {
-        std::function<void()> event;
-        while (disposer->load() == false) {
-            if (tasks_.try_pop(event)) {
-                num_working_workers_.fetch_add(1);
-                event();
-                num_working_workers_.fetch_sub(1);
-            }
-            else {
-                std::unique_lock<std::mutex> lock(event_lock_);
-                event_wait_.wait(lock);
-            }
-        }
-    };
-
-    workers_.emplace_back(disposer, std::thread(std::move(worker)));
-    return true;
-}
-
-inline void thread_pool::pop_workers__(size_t count)
-{
-    auto const begin = workers_.end() - count;
-    auto const end = workers_.end();
-
-    for (auto it = begin; it != end; ++it) {
-        it->first->store(true);
-    }
-    event_wait_.notify_all();
-    for (auto it = begin; it != end; ++it) {
-        it->second.join();
-    }
-
-    workers_.erase(begin, end);
-}
 
 template <typename Fn_, typename... Args_>
 decltype(auto) thread_pool::launch_task(Fn_&& f, Args_... args)
@@ -176,12 +123,122 @@ decltype(auto) thread_pool::launch_task(Fn_&& f, Args_... args)
         std::this_thread::yield();
     }
 
-    if (num_available_workers() == 0) {
-        std::lock_guard<std::mutex> lock(worker_lock_);
-        try_add_worker__();
-    }
-
     event_wait_.notify_one();
     return return_type(promise->get_future());
+}
+
+inline thread_pool::thread_pool(size_t task_queue_cap_, size_t num_workers, size_t worker_limit) noexcept
+    : tasks_(task_queue_cap_)
+    , worker_limit_(worker_limit)
+{
+    resize_worker_pool(num_workers);
+
+    launch_task([this]() {
+        while (!workers_.front().disposer.value) {
+            if (num_available_workers() == 0) {
+                std::lock_guard<std::mutex> lock{worker_lock_};
+                bool should_spawn_worker;
+                {
+                    std::chrono::system_clock::time_point max_tp = {};
+                    for (auto& worker : workers_) {
+                        max_tp = std::max(max_tp, worker.last_active);
+                    }
+
+                    should_spawn_worker
+                      = std::chrono::system_clock::now() - max_tp > stall_wait_tolerance;
+                }
+
+                if (should_spawn_worker) {
+                    // Extend threads 2x
+                    for (size_t count = workers_.size();
+                         count-- && workers_.size() < worker_limit_;
+                         (void)0) {
+                        try_add_worker__();
+                    }
+                }
+            }
+
+            std::this_thread::sleep_for(stall_check_period);
+        }
+    });
+}
+
+inline thread_pool::~thread_pool()
+{
+    pop_workers__(workers_.size());
+}
+
+inline void thread_pool::resize_worker_pool(size_t new_size)
+{
+    if (new_size == 0) {
+        throw std::invalid_argument{"Size 0 is not allowed"};
+    }
+    new_size += 1; // Reserve first
+
+    std::lock_guard<std::mutex> lock(worker_lock_);
+    if (new_size > workers_.size()) {
+        new_size = std::min(worker_limit_, new_size);
+        while (new_size != workers_.size()) {
+            try_add_worker__();
+        }
+    }
+    else if (new_size < workers_.size()) {
+        pop_workers__(workers_.size() - new_size);
+    }
+}
+
+inline size_t thread_pool::num_workers() const
+{
+    std::lock_guard<std::mutex> lock(worker_lock_);
+    return workers_.size();
+}
+
+inline bool thread_pool::try_add_worker__()
+{
+    if (workers_.size() >= worker_limit_) {
+        return false;
+    }
+
+    auto worker = [this, index = workers_.size()]() {
+        std::function<void()> event;
+        auto& desc = workers_[index];
+
+        while (desc.disposer.value == false) {
+            if (tasks_.try_pop(event)) {
+                desc.last_active = std::chrono::system_clock::now();
+
+                num_working_workers_.fetch_add(1);
+                event();
+                num_working_workers_.fetch_sub(1);
+            }
+            else {
+                std::unique_lock<std::mutex> lock(event_lock_);
+                event_wait_.wait(lock);
+            }
+        }
+    };
+
+    auto& wd = workers_.emplace_back();
+    wd.disposer.value = false;
+    wd.thread = std::thread(std::move(worker));
+    wd.last_active = {};
+
+    return true;
+}
+
+inline void thread_pool::pop_workers__(size_t count)
+{
+    auto const begin = workers_.end() - count;
+    auto const end = workers_.end();
+
+    for (auto it = begin; it != end; ++it) {
+        it->disposer.value.store(true);
+    }
+    event_wait_.notify_all();
+    for (auto it = begin; it != end; ++it) {
+        it->thread.join();
+    }
+
+    workers_.erase(begin, end);
 }
 } // namespace templates
--- tests/src/test-thread_pool.cpp
+++ tests/src/test-thread_pool.cpp
@@ -7,9 +7,9 @@ using namespace std;
 TEST_CASE("thread pool default operation", "[thread_pool]")
 {
     printf("THREAD POOL TEST --- \n");
-    size_t num_cases = 1022;
+    size_t num_cases = 1024;
 
-    thread_pool thr{1024};
+    thread_pool thr{1024, 1};
     vector<pair<double, future<double>>> futures;
 
     futures.reserve(num_cases);
@@ -18,7 +18,7 @@ TEST_CASE("thread pool default operation", "[thread_pool]")
         futures.emplace_back(
           i, thr.launch_task(
                [](double c) {
-                   this_thread::sleep_for(1ms);
+                   this_thread::sleep_for(50ms);
                    putchar('.');
                    return c * c;
                },
@@ -26,15 +26,13 @@ TEST_CASE("thread pool default operation", "[thread_pool]")
     }
 
     size_t num_error = 0;
-    thr.resize_worker_pool(1);
-    std::this_thread::sleep_for(1000ms);
-    thr.resize_worker_pool(24);
-    REQUIRE(thr.num_workers() == 24);
 
     for (auto& pair : futures) {
         num_error += pair.first * pair.first != pair.second.get();
     }
 
+    printf("\nNUM_THR: %llu", thr.num_workers());
+    CHECK(thr.num_workers() != 2);
     REQUIRE(num_error == 0);
 
     printf("\n");

9223036930ce9d256c2542ceee27fc2d054f5c90
Seungwoo Kang <85146120+pxscope-swkang@users.noreply.github.com>
2021-08-12 10:25:55 +0900

    Add new macro for string formatting

--- core/include/pipepp/pipepp.h
+++ core/include/pipepp/pipepp.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <sstream>
 #include "pipepp/impl/pipeline.hxx"
 #include "pipepp/options.hpp"
-#include <sstream>
 
 namespace pipepp {
 using options = const detail::option_base;
@@ -109,6 +109,10 @@ using options = const detail::option_base;
 #define PIPEPP_STORE_DEBUG_DATA(NAME, VALUE) ___PIPEPP_STORE_DEBUG_DATA(NAME, VALUE)
 #define PIPEPP_STORE_DEBUG_DATA_DYNAMIC_STR(NAME, VALUE) ___call_PIPEPP_REGISTER_CONTEXT.store_debug_data(NAME, (std::stringstream{} << VALUE).str());
 #define PIPEPP_STORE_DEBUG_STR(NAME, VALUE) ___PIPEPP_STORE_DEBUG_DATA(NAME, (std::stringstream{} << VALUE).str())
+
+#define PIPEPP_STORE_DEBUG_DATA_DYNAMIC_STRF(NAME, FMT, ...) ___call_PIPEPP_REGISTER_CONTEXT.store_debug_data(NAME, std::format((FMT), ##__VA_ARGS__));
+#define PIPEPP_STORE_DEBUG_STRF(NAME, FMT, ...) ___PIPEPP_STORE_DEBUG_DATA(NAME, std::format((FMT), ##__VA_ARGS__))
+
 #define PIPEPP_CAPTURE_DEBUG_DATA(VALUE) ___PIPEPP_STORE_DEBUG_DATA(#VALUE, VALUE)
 #define PIPEPP_STORE_DEBUG_DATA_COND(NAME, VALUE, COND) ___PIPEPP_STORE_DEBUG_DATA_COND(NAME, VALUE, COND)
 #define PIPEPP_CAPTURE_DEBUG_DATA_COND(VALUE, COND) ___PIPEPP_STORE_DEBUG_DATA_COND(#VALUE, VALUE, COND)
